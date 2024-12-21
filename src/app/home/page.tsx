23e1666f410fdc4000b2788421d676b4592887e9
sriraml123 <sriramk.lnrstechnologies@gmail.com>
2024-12-21 23:00:11 +0530

    exam portal

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -26,7 +26,7 @@ const ExamPortal = () => {
     return () => clearInterval(timer);
   }, [examStarted, timeLeft]);
 
-  const formatTime = (seconds) => {
+  const formatTime = (seconds: number) => {
     const mins = Math.floor(seconds / 60);
     const secs = seconds % 60;
     return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
